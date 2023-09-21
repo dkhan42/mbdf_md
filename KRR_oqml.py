618edcebafac410a43c5b2dbd91ad117c84fe484
dkhan42 <66374468+dkhan42@users.noreply.github.com>
2023-09-21 10:35:18 -0400

    Update KRR_oqml.py

--- KRR_oqml.py
+++ KRR_oqml.py
@@ -2,6 +2,7 @@ import numpy as np
 from qml.math import svd_solve
 from scipy.linalg import cho_solve
 from qml.kernels import get_atomic_local_kernel, get_atomic_local_gradient_kernel
+from qml.kernels import get_local_gradient_kernel, get_local_kernel
 
 def KRR_oqml(xt, dxt, qt, et, ft, xte, dxte, qte, sigma, lam):
     kte = get_atomic_local_kernel(xt, xt, qt, qt, sigma)
@@ -15,3 +16,19 @@ def KRR_oqml(xt, dxt, qt, et, ft, xte, dxte, qte, sigma, lam):
     eYt = np.dot(Ks_energy, alpha)
     fYt = np.dot(Ks_force, alpha)
     return eYt, fYt
+
+def krr_local(rtrain, drtrain, qt, ftr, rtest, drtest, qtest, sigma, lam):
+    ktr_local_grad = get_local_gradient_kernel(rtrain, rtrain, drtrain, 
+                                                   qt, qt, sigma)
+    alpha_local = svd_solve(ktr_local_grad, ftr.flatten().astype(float), rcond=lam)
+    kte_local_grad = get_local_gradient_kernel(rtrain, rtest, drtest, 
+                                           qt, qtest, sigma)
+    return np.dot(kte_local_grad, alpha_local)
+
+def krr_atomic(rtrain, drtrain, qt, ftr, rtest, drtest, qtest, sigma, lam):
+    ktr_local_grad = get_atomic_local_gradient_kernel(rtrain, rtrain, drtrain, 
+                                                   qt, qt, sigma)
+    alpha_local = svd_solve(ktr_local_grad, ftr.flatten().astype(float), rcond=lam)
+    kte_local_grad = get_atomic_local_gradient_kernel(rtrain, rtest, drtest, 
+                                           qt, qtest, sigma)
+    return np.dot(kte_local_grad, alpha_local)
